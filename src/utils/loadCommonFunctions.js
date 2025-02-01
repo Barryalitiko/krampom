a45b97ed490f12cb4c9da9eb9af9606f5f7d9592
Krampus B. <tiobarryx@gmail.com>
2025-02-01 22:57:43 +0100

    Update loadCommonFunctions.js

--- src/utils/loadCommonFunctions.js
+++ src/utils/loadCommonFunctions.js
@@ -1,12 +1,4 @@
-/**
- * Funções comuns de uso geral
- * do bot. Não há
- * necessidade de modificar
- * este arquivo.
- *
- * @author Dev Gui
- */
-const { BOT_EMOJI } = require("../config");
+const { BOT_EMOJI } = require("../krampus");
 const { extractDataFromMessage, baileysIs, download } = require(".");
 const { waitMessage } = require("./messages");
 const fs = require("fs");
@@ -27,11 +19,12 @@ exports.loadCommonFunctions = ({ socket, webMessage }) => {
   if (!remoteJid) {
     return null;
   }
-
+  // Detección de tipos de medios
   const isImage = baileysIs(webMessage, "image");
   const isVideo = baileysIs(webMessage, "video");
   const isSticker = baileysIs(webMessage, "sticker");
 
+  // Funciones para descargar los archivos según el tipo
   const downloadImage = async (webMessage, fileName) => {
     return await download(webMessage, fileName, "image", "png");
   };
@@ -44,6 +37,27 @@ exports.loadCommonFunctions = ({ socket, webMessage }) => {
     return await download(webMessage, fileName, "video", "mp4");
   };
 
+  // Función para manejar los medios si se activa
+  const handleMediaMessage = async (processMedia) => {
+    if (!processMedia) return; // Solo procesa si se activa específicamente
+
+    if (isImage) {
+      console.log("Procesando imagen...");
+      const imagePath = await downloadImage(webMessage, "image");
+      return { type: "image", path: imagePath };
+    }
+
+    if (isVideo) {
+      console.log("Procesando video...");
+      const videoPath = await downloadVideo(webMessage, "video");
+      return { type: "video", path: videoPath };
+    }
+
+    console.log("No se detectó imagen ni video.");
+    return null;
+  };
+
+  // Funciones para enviar textos y respuestas
   const sendText = async (text, mentions) => {
     let optionalParams = {};
 
@@ -57,6 +71,7 @@ exports.loadCommonFunctions = ({ socket, webMessage }) => {
     });
   };
 
+  // Función para enviar respuesta a un mensaje
   const sendReply = async (text) => {
     return await socket.sendMessage(
       remoteJid,
@@ -65,6 +80,7 @@ exports.loadCommonFunctions = ({ socket, webMessage }) => {
     );
   };
 
+  // Funciones para reacciones comunes
   const sendReact = async (emoji) => {
     return await socket.sendMessage(remoteJid, {
       react: {
@@ -77,13 +93,26 @@ exports.loadCommonFunctions = ({ socket, webMessage }) => {
   const sendSuccessReact = async () => {
     return await sendReact("✅");
   };
+  
+    const sendPuzzleReact = async () => {
+    return await sendReact("🧩");
+  };
 
-  const sendWaitReact = async () => {
-    return await sendReact("⏳");
+  const sendMusicReact = async () => {
+    return await sendReact("🎵");
+  };
+
+  const sendWarningReply = async (text) => {
+    await sendWarningReact();
+    return await sendReply(`⚠️ Advertencia! ${text}`);
   };
 
   const sendWarningReact = async () => {
     return await sendReact("⚠️");
+  };
+
+  const sendWaitReact = async () => {
+    return await sendReact("⏳");
   };
 
   const sendErrorReact = async () => {
@@ -92,22 +121,45 @@ exports.loadCommonFunctions = ({ socket, webMessage }) => {
 
   const sendSuccessReply = async (text) => {
     await sendSuccessReact();
-    return await sendReply(`✅ ${text}`);
+    return await sendReply(`👻 ${text}`);
   };
 
   const sendWaitReply = async (text) => {
     await sendWaitReact();
-    return await sendReply(`⏳ Aguarde! ${text || waitMessage}`);
-  };
-
-  const sendWarningReply = async (text) => {
-    await sendWarningReact();
-    return await sendReply(`⚠️ Atenção! ${text}`);
+    return await sendReply(`⏳ Espera! ${text || waitMessage}`);
   };
 
   const sendErrorReply = async (text) => {
     await sendErrorReact();
-    return await sendReply(`❌ Erro! ${text}`);
+    return await sendReply(`☠ Error! ${text}`);
+  };
+
+  const sendAudioFromURL = async (url) => {
+    try {
+      console.log(`Enviando audio desde URL: ${url}`);
+      return await socket.sendMessage(
+        remoteJid,
+        {
+          audio: { url },
+          mimetype: "audio/mpeg",
+        },
+        { quoted: webMessage }
+      );
+    } catch (error) {
+      console.error("Error al enviar el audio:", error);
+      throw new Error("No se pudo enviar el audio.");
+    }
+  };
+
+  const sendVideoFromURL = async (url) => {
+    console.log(`Enviando video desde URL: ${url}`);
+    return await socket.sendMessage(
+      remoteJid,
+      {
+        video: { url },
+      },
+      { quoted: webMessage }
+    );
   };
 
   const sendStickerFromFile = async (file) => {
@@ -130,6 +182,37 @@ exports.loadCommonFunctions = ({ socket, webMessage }) => {
     );
   };
 
+  const sendMessage = async ({ messageType, caption = '', mimetype = '', url = '' }) => {
+    try {
+      let messageContent = {};
+
+      if (messageType === 'audio') {
+        messageContent = { audio: { url }, mimetype };
+      } else if (messageType === 'video') {
+        messageContent = { video: { url }, caption, mimetype };
+      } else if (messageType === 'image') {
+        messageContent = { image: { url }, caption, mimetype };
+      }
+
+      await socket.sendMessage(remoteJid, messageContent, { quoted: webMessage });
+      console.log(`${messageType} enviado con éxito.`);
+    } catch (error) {
+      console.error(`Error al enviar el mensaje de tipo ${messageType}:`, error);
+    }
+  };
+
+  const sendVideoFromFile = async (filePath, caption = '') => {
+    console.log(`Enviando video desde archivo: ${filePath}`);
+    return await socket.sendMessage(
+      remoteJid,
+      {
+        video: fs.readFileSync(filePath),
+        caption: caption,
+      },
+      { quoted: webMessage }
+    );
+  };
+
   const sendImageFromFile = async (file, caption = "") => {
     return await socket.sendMessage(
       remoteJid,
@@ -140,8 +223,8 @@ exports.loadCommonFunctions = ({ socket, webMessage }) => {
       { quoted: webMessage }
     );
   };
-
-  const sendImageFromURL = async (url, caption = "") => {
+  
+    const sendImageFromURL = async (url, caption = "") => {
     return await socket.sendMessage(
       remoteJid,
       {
@@ -152,61 +235,46 @@ exports.loadCommonFunctions = ({ socket, webMessage }) => {
     );
   };
 
-  const sendAudioFromURL = async (url) => {
-    return await socket.sendMessage(
-      remoteJid,
-      {
-        audio: { url },
-        mimetype: "audio/mp4",
-      },
-      { url, quoted: webMessage }
-    );
-  };
-
-  const sendVideoFromURL = async (url) => {
-    return await socket.sendMessage(
-      remoteJid,
-      {
-        video: { url },
-      },
-      { url, quoted: webMessage }
-    );
-  };
 
   return {
     args,
     commandName,
-    fullArgs,
-    fullMessage,
-    isImage,
-    isReply,
-    isSticker,
-    isVideo,
-    prefix,
-    remoteJid,
-    replyJid,
-    socket,
-    userJid,
-    webMessage,
     downloadImage,
     downloadSticker,
     downloadVideo,
-    sendAudioFromURL,
-    sendErrorReact,
-    sendErrorReply,
+    fullArgs,
+    fullMessage,
+    handleMediaMessage,
+    isReply,
+    isSticker,
+    isVideo,
+    isImage,
+    prefix,
+    remoteJid,
+    replyJid,
+    sendText,
+    sendReply,
+    socket,
+    userJid,
+    webMessage,
+    sendReact,
+    sendPuzzleReact,
     sendImageFromFile,
     sendImageFromURL,
-    sendReact,
-    sendReply,
+    sendSuccessReact,
+    sendMusicReact,
+    sendWarningReply,
+    sendWarningReact,
+    sendWaitReact,
+    sendErrorReact,
+    sendSuccessReply,
+    sendWaitReply,
+    sendErrorReply,
+    sendAudioFromURL,
+    sendVideoFromURL,
     sendStickerFromFile,
     sendStickerFromURL,
-    sendSuccessReact,
-    sendSuccessReply,
-    sendText,
-    sendVideoFromURL,
-    sendWaitReact,
-    sendWaitReply,
-    sendWarningReact,
-    sendWarningReply,
+    sendMessage,
+    sendVideoFromFile,
   };
 };
