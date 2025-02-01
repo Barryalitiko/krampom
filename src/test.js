ee7745c36c3663d47e4a7fef9939c9247e623a85
Krampus B. <tiobarryx@gmail.com>
2025-02-01 23:06:17 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,9 +1,3 @@
-/**
- * Arquivo pra rodar alguns testes,
- * nada demais.
- *
- * @author Dev Gui
- */
 (async () => {
-  console.log("Apenas testando");
+  console.log("Iniciando test . . .");
 })();
