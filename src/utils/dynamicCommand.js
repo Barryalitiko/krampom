e09c1cf5f399367d0ac504178b06f6f3c137f468
Krampus B. <tiobarryx@gmail.com>
2025-02-01 22:55:35 +0100

    Update dynamicCommand.js

--- src/utils/dynamicCommand.js
+++ src/utils/dynamicCommand.js
@@ -1,116 +1,91 @@
-/**
- * Direcionador
- * de comandos.
- *
- * @author Dev Gui
- */
 const { DangerError } = require("../errors/DangerError");
 const { InvalidParameterError } = require("../errors/InvalidParameterError");
 const { WarningError } = require("../errors/WarningError");
 const { findCommandImport } = require(".");
 const {
-  verifyPrefix,
-  hasTypeOrCommand,
-  isLink,
-  isAdmin,
+verifyPrefix,
+hasTypeOrCommand,
+isLink,
+isAdmin,
 } = require("../middlewares");
 const { checkPermission } = require("../middlewares/checkPermission");
-const {
-  isActiveGroup,
-  getAutoResponderResponse,
-  isActiveAutoResponderGroup,
-  isActiveAntiLinkGroup,
-} = require("./database");
+const { isActiveAntiLinkGroup, getAntiLinkMode } = require("./database");
 const { errorLog } = require("../utils/logger");
-const { ONLY_GROUP_ID } = require("../config");
+const { ONLY_GROUP_ID } = require("../krampus");
+
+function isGroupLink(message) {
+return message.includes("chat.whatsapp.com");
+}
 
 exports.dynamicCommand = async (paramsHandler) => {
-  const {
-    commandName,
-    prefix,
-    sendWarningReply,
-    sendErrorReply,
-    remoteJid,
-    sendReply,
-    socket,
-    userJid,
-    fullMessage,
-    webMessage,
-  } = paramsHandler;
+const {
+commandName,
+prefix,
+sendWarningReply,
+sendErrorReply,
+remoteJid,
+sendReply,
+socket,
+userJid,
+fullMessage,
+webMessage,
+} = paramsHandler;
 
-  if (isActiveAntiLinkGroup(remoteJid) && isLink(fullMessage)) {
-    if (!userJid) return;
+if (isActiveAntiLinkGroup(remoteJid) && isLink(fullMessage)) {
+const antiLinkMode = getAntiLinkMode(remoteJid);
+if (
+antiLinkMode === "2" ||
+(antiLinkMode === "1" && isGroupLink(fullMessage))
+) {
+if (!(await isAdmin({ remoteJid, userJid, socket }))) {
+await socket.groupParticipantsUpdate(remoteJid, [userJid], "remove");
+await sendReply(
+"👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Baneado por enviar link"
+);
+await socket.sendMessage(remoteJid, {
+delete: {
+remoteJid,
+fromMe: false,
+id: webMessage.key.id,
+participant: webMessage.key.participant,
+},
+});
+return;
+}
+}
+}
 
-    if (!(await isAdmin({ remoteJid, userJid, socket }))) {
-      await socket.groupParticipantsUpdate(remoteJid, [userJid], "remove");
+const { type, command } = findCommandImport(commandName);
 
-      await sendReply(
-        "Anti-link ativado! Você foi removido por enviar um link!"
-      );
+if (ONLY_GROUP_ID && ONLY_GROUP_ID !== remoteJid) {
+return;
+}
 
-      await socket.sendMessage(remoteJid, {
-        delete: {
-          remoteJid,
-          fromMe: false,
-          id: webMessage.key.id,
-          participant: webMessage.key.participant,
-        },
-      });
+if (!verifyPrefix(prefix) || !hasTypeOrCommand({ type, command })) {
+return;
+}
 
-      return;
-    }
-  }
+if (!(await checkPermission({ type, ...paramsHandler }))) {
+await sendErrorReply(
+"👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 No tienes permitido usar el comando"
+);
+return;
+}
 
-  const { type, command } = findCommandImport(commandName);
-
-  if (ONLY_GROUP_ID && ONLY_GROUP_ID !== remoteJid) {
-    return;
-  }
-
-  if (!verifyPrefix(prefix) || !hasTypeOrCommand({ type, command })) {
-    if (isActiveAutoResponderGroup(remoteJid)) {
-      const response = getAutoResponderResponse(fullMessage);
-
-      if (response) {
-        await sendReply(response);
-      }
-    }
-
-    return;
-  }
-
-  if (!(await checkPermission({ type, ...paramsHandler }))) {
-    await sendErrorReply("Você não tem permissão para executar este comando!");
-    return;
-  }
-
-  if (!isActiveGroup(remoteJid) && command.name !== "on") {
-    await sendWarningReply(
-      "Este grupo está desativado! Peça para o dono do grupo ativar o bot!"
-    );
-
-    return;
-  }
-
-  try {
-    await command.handle({
-      ...paramsHandler,
-      type,
-    });
-  } catch (error) {
-    if (error instanceof InvalidParameterError) {
-      await sendWarningReply(`Parâmetros inválidos! ${error.message}`);
-    } else if (error instanceof WarningError) {
-      await sendWarningReply(error.message);
-    } else if (error instanceof DangerError) {
-      await sendErrorReply(error.message);
-    } else {
-      errorLog("Erro ao executar comando", error);
-      await sendErrorReply(
-        `Ocorreu um erro ao executar o comando ${command.name}! O desenvolvedor foi notificado!
-      
-📄 *Detalhes*: ${error.message}`
-      );
-    }
-  }
+try {
+await command.handle({ ...paramsHandler, type });
+} catch (error) {
+if (error instanceof InvalidParameterError) {
+await sendWarningReply(`Parametros inválidos! ${error.message}`);
+} else if (error instanceof WarningError) {
+await sendWarningReply(error.message);
+} else if (error instanceof DangerError) {
+await sendErrorReply(error.message);
+} else {
+errorLog("Error al ejecutar el comando", error);
+await sendErrorReply(
+`👻 𝙺𝚛𝚊𝚖𝚙𝚞𝚜.𝚋𝚘𝚝 👻 Ocurrio un error al ejecutar el comando ${command.name}! 📄 *Detalles*: ${error.message}`
+);
+}
+}
 };
